3d4144361fb5316a6a29af0373ac4cd4b4eaeb7f
ana castaneda <anita_1017@hotmail.com>
2022-11-18 17:53:51 +0100

    Ex2

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,8 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
+  has_one_attached :photo
   has_many :movies, through: :bookmarks, dependent: :destroy # check if this thoruws error
   validates :name, presence: true
   validates :name, uniqueness: true
+  validates :photo, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,19 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 require "faker"
+require "open-uri"
 
 List.destroy_all
 
-List.create(name: "Drama")
-List.create(name: "Terror")
-List.create(name: "Comedy")
-List.create(name: "Romance")
+# List.create(name: "Drama")
+# List.create(name: "Terror")
+# List.create(name: "Comedy")
+# List.create(name: "Romance")
+
+# file = URI.open("https://media.istockphoto.com/id/1191001701/photo/popcorn-and-clapperboard.jpg?s=612x612&w=0&k=20&c=iUkFTVuU8k-UCcZDxczTWs6gkRa0nAMihp2Jf_2ASKM=")
+# list = List.new(name: "Drama")
+# list.photo.attach(io: file, filename: "Drama.png", content_type: "image/png")
+# list.save
 
 Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
 Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
